4ba97a58ec292bad6ff429e5860b1b3c349c0062
marianaServan <servan.mariana@gmail.com>
2021-11-25 20:22:16 -0300

    Desafio jQuery

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ let cantidad
 let costo = 0
 let contador = 1
 let carritoAlmacenado
+let carritoGuardado
+let costoAlmacenado
+let costoGuardado
 
 // Objetos
 class Producto {
@@ -29,8 +32,25 @@ class Producto {
         this.tela = tela;
     }
 
-    descripcion() {
-        return (`Disponible: ${this.nombre} color ${this.color}, de ${this.tela}. <br> Precio: $${this.precio}`)
+    cardProducto() {
+        return `
+            <div class="col-3"> 
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"> ${this.nombre.toUpperCase()}</h5>
+                        <p class="card-text">
+                            Disponible: ${this.nombre} color ${this.color}, de ${this.tela}. <br> Precio: $${this.precio}
+                        </p>
+                        <div class="input-group">
+                            <input type="number" class="form-control" placeholder="1" id="cantidad${this.nombre}" name="cantidad${this.nombre}" value="1">
+                            <button class="btn btn-outline-secondary" type="button" id="botonMas${this.nombre}" name="botonMas${this.nombre}">+</button>
+                            <button class="btn btn-outline-secondary" type="button" id="botonAgrega${this.nombre}" name="botonAgrega${this.nombre}">Agregar al
+                            carrito</button>
+                        </div>                    
+                    </div>
+                </div>
+            </div>
+        `
     }
 }
 
@@ -42,78 +62,80 @@ const camperaAzul = new Producto("campera", 3500, "azul francia", "frisa de algo
 const productos = [remeraBlanca, sweaterGris, camperaAzul]
 const carritoUsuario = []
 
-// Agrego los productos disponibles al HTML
-productos.forEach(producto => {
-    let cardProducto = document.createElement("div")
-    cardProducto.className = "col-3"
-    cardProducto.innerHTML = `
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title"> ${producto.nombre.toUpperCase()}</h5>
-                <p class="card-text"> ${producto.descripcion()}</p>
-                <div class="input-group">
-                    <input type="number" class="form-control" placeholder="1" id="cantidad${producto.nombre}" name="cantidad${producto.nombre}" value="1">
-                    <button class="btn btn-outline-secondary" type="button" id="mas${producto.nombre}" name="mas${producto.nombre}">+</button>
-                    <button class="btn btn-outline-secondary" type="button" id="agrega${producto.nombre}" name="agrega${producto.nombre}">Agregar al
-                    carrito</button>
-                </div>                    
-            </div>
-        </div>
-    `
-    divProductos.appendChild(cardProducto) //agrega el elemento al DOM
-})
-
-// Agrego al carrito
-let divAgregadoCorrecto = document.getElementById("divAgregadoCorrecto")
-let divCarrito = document.getElementById("divCarrito")
-
-let carritoGuardado = localStorage.setItem('Carrito', JSON.stringify(carritoUsuario))
-productos.forEach(producto => {
-    let cantidadProd = document.getElementById(`cantidad${producto.nombre}`)
-    let mas = document.getElementById(`mas${producto.nombre}`)
-    let agrega = document.getElementById(`agrega${producto.nombre}`)
-    
-    mas.addEventListener("click", () => {
-        contador = parseInt(cantidadProd.value) + 1
-        cantidadProd.value= `${contador}`
+$(() => {
+    // Agrego los productos disponibles al HTML
+    productos.forEach(producto => {
+        $('#divProductos').append(producto.cardProducto());
     })
 
-    agrega.addEventListener("click", () => {
-        cantidad = parseInt(cantidadProd.value)
-        carritoUsuario.push({productoComprado: producto, cantidadComprada: cantidad})
-        carritoGuardado = localStorage.setItem('Carrito', JSON.stringify(carritoUsuario))
-        costo += costoTotal(producto.precio, cantidad)
-    
-        divAgregadoCorrecto.innerHTML = `
-            <p> Se ha agregado correctamente ${cantidad} ${producto.nombre} al carrito. </p>
-        `    
-        divCarrito.innerHTML = ``
-    })
-})
-
-
-
-// Muestro el carrito
-let verCarrito = document.getElementById("verCarrito")
-verCarrito.addEventListener("click", () => {
-    divCarrito.innerHTML = ``    
+    // Veo si dejó el carrito cargado de otra sesión
     carritoAlmacenado = JSON.parse(localStorage.getItem('Carrito'))
-    if (carritoAlmacenado.length == 0) {
-        let carritoVacio = document.createElement("p")
-        carritoVacio.innerText = "No hay productos en el carrito"
-        divCarrito.appendChild(carritoVacio) 
-    } else {
-        let contenidoCarrito = document.createElement("p")
-        contenidoCarrito.innerText = "En el carrito:" 
-        divCarrito.appendChild(contenidoCarrito) 
-
+    if (carritoAlmacenado.length != 0) {
         carritoAlmacenado.forEach(compra => {
-            let productoEnCarrito = document.createElement("p")
-            productoEnCarrito.innerText = `${compra.productoComprado.nombre} ----- ${compra.cantidadComprada} x $${compra.productoComprado.precio}`
-            divCarrito.appendChild(productoEnCarrito) 
+            carritoUsuario.push({ productoComprado: compra.productoComprado, cantidadComprada: compra.cantidadComprada })
         })
-        let detalleCosto = document.createElement("p") 
-        detalleCosto.innerText = `${costoFinal(costo)}` 
-        divCarrito.appendChild(detalleCosto) 
     }
+
+    costoAlmacenado = JSON.parse(localStorage.getItem('Costo'))
+    if (costoAlmacenado != 0) {
+        costo = costoAlmacenado
+    }
+
+    // Agrego al carrito
+    productos.forEach(producto => {
+        let cantidadProd = $(`#cantidad${producto.nombre}`)
+
+        $(`#botonMas${producto.nombre}`).click(() => {
+            contador = parseInt(cantidadProd.val()) + 1
+            cantidadProd.val(`${contador}`)
+        })
+
+        $(`#botonAgrega${producto.nombre}`).click(() => {
+            cantidad = parseInt(cantidadProd.val())
+            carritoUsuario.push({ productoComprado: producto, cantidadComprada: cantidad })
+            carritoGuardado = localStorage.setItem('Carrito', JSON.stringify(carritoUsuario))
+
+            costo += costoTotal(producto.precio, cantidad)
+            costoGuardado = localStorage.setItem('Costo', JSON.stringify(costo))
+
+            $('#divAgregadoCorrecto').empty()
+            $('#divAgregadoCorrecto').append(`
+                <p> Se ha agregado correctamente ${cantidad} ${producto.nombre} al carrito. </p>
+            `
+            )
+            $('#divCarrito').empty()
+        })
+    })
+
+    // Muestro el carrito
+    $('#verCarrito').click(() => {
+        $('#divCarrito').empty()
+        carritoAlmacenado = JSON.parse(localStorage.getItem('Carrito'))
+        costoAlmacenado = JSON.parse(localStorage.getItem('Costo'))
+
+        if (carritoAlmacenado.length == 0) {
+            $('#divCarrito').append(`
+                <p> No hay productos en el carrito.</p>
+            `
+            )
+        } else {
+            $('#divCarrito').append(`
+                <p> En el carrito:</p>
+            `
+            )
+
+            carritoAlmacenado.forEach(compra => {
+                $('#divCarrito').append(`
+                <p> ${compra.productoComprado.nombre} ----- ${compra.cantidadComprada} x $${compra.productoComprado.precio} </p>
+                `
+                )
+            })
+
+            $('#divCarrito').append(`
+                <p> ${costoFinal(costoAlmacenado)} </p>
+            `
+            )
+        }
+    })
 })
+
