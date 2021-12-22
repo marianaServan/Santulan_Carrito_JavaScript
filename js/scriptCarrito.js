001a815d878cae077fc596214ba185d7ae0bb43e
marianaServan <servan.mariana@gmail.com>
2021-12-22 01:42:51 -0300

    Carrito a la derecha, un solo script, y modificación de cantidades en carrito

--- js/scriptCarrito.js
+++ js/scriptCarrito.js
@@ -1,8 +1,13 @@
 // Funciones
+function subtotal(productoEnCarrito) {
+    let subT = productoEnCarrito[0].precio * productoEnCarrito[1]
+    return subT
+}
+
 function costoFinal(carrito) {
     let costoF = 0
-    carrito.forEach(producto => {
-        costoF += producto.precio
+    carrito.forEach(productoEnCarrito => {
+        costoF += subtotal(productoEnCarrito)
     })
     return costoF
 }
@@ -17,73 +22,110 @@ function detalleCostoFinal(costo) {
     }
 }
 
+function detalleCarrito(carrito) {
+    if (carrito.length == 0) {
+        return (`
+            <div class="centradoHoriz"> 
+                <p class="textGenParrafoCarrito"> No hay productos en el carrito.</p>
+                <button class="botonesCarrito textGenParrafoCarrito" type="button" id="botonFinalizarCompra" name="botonFinalizarCompra"> Finalizar compra</button>
+            </div>
+        `
+        )
+    } else {
+        return (`
+            <div class="centradoHoriz"> 
+                <p class="textGenParrafoCarrito"> ${detalleCostoFinal(costoFinal(carrito))} </p>
+                <button class="textGenParrafo botonesCarrito" type="button" data-bs-toggle="offcanvas"
+                                data-bs-target="#offcanvasRight-Form" aria-controls="offcanvasRight-Form"> 
+                                finalizar compra </button>
+            </div>
+        `
+        )
+    }
+}
+
+function eliminarProducto(indice, carrito) {
+    $('#divCarrito').empty()
+    carrito.splice(indice, 1)
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+    $(`#productoCarrito${indice}`).remove()
+    $('#divCarrito').append(detalleCarrito(carrito))
+}
+
+function actualizoCostos(productoEnCarrito, carritoUsuario) {
+    $(`#subtotal${productoEnCarrito[0].id}`).empty()
+    $(`#subtotal${productoEnCarrito[0].id}`).append(`${subtotal(productoEnCarrito)}`)
+    $('#divCarrito').empty()
+    $('#divCarrito').append(detalleCarrito(carritoUsuario))
+}
+
 // Variables
 let carritoUsuario
-let contador
-let cantidad
-let costo
-
+let valor
 
 $(() => {
+    // Consulto si tengo algo guardado en el localStorage
     carritoUsuario = JSON.parse(localStorage.getItem('carrito'))
     if (carritoUsuario == null) {
         carritoUsuario = []
     }
 
-    carritoUsuario.forEach((producto, indice) => {
+    // armo el carrito
+    carritoUsuario.forEach((productoEnCarrito, indice) => {
         $('#tablaCarrito').append(`
             <tr id="productoCarrito${indice}">
-                <th scope="row" class="textGenParrafo">
-                    ${producto.nombre} ${producto.color}, ${producto.detalle}
+                <th scope="row" class="textGenParrafoCarrito">
+                    ${productoEnCarrito[0].nombre} ${productoEnCarrito[0].color}, ${productoEnCarrito[0].detalle}
                     </br>
-                    <input class="btn btn-outline-secondary" type="button" id="cantidad${producto.id}" name="cantidad${producto.id}" value="1">                        
-                    <button class="btn btn-outline-secondary" type="button" id="botonEliminar${producto.id}" name="botonMas${producto.id}"><i class="bi bi-trash"></i></button>
+                    <button class="botonesDelCarrito" type="button" id="botonMenos${productoEnCarrito[0].id}" name="botonMenos${productoEnCarrito[0].id}"><i class="bi bi-dash"></i></button>
+                    <input class="botonesDelCarrito" type="button" id="cantidad${productoEnCarrito[0].id}" name="cantidad${productoEnCarrito[0].id}" value="${productoEnCarrito[1]}">                        
+                    <button class="botonesDelCarrito" type="button" id="botonMas${productoEnCarrito[0].id}" name="botonMas${productoEnCarrito[0].id}"><i class="bi bi-plus"></i></button>
+                    <button class="botonesDelCarrito" type="button" id="botonEliminar${productoEnCarrito[0].id}" name="botonEliminar${productoEnCarrito[0].id}"><i class="bi bi-trash"></i></button>
                 </th>
-                <th scope="row" class="textGenParrafo"> ${producto.precio} </th>                    
+                <th scope="row" class="textGenParrafoCarrito" id="subtotal${productoEnCarrito[0].id}"> ${subtotal(productoEnCarrito)} </th>                    
             </tr>
         `
         )
 
-        $(`#botonEliminar${producto.id}`).click(() => {
-            $('#divCarrito').empty()
-            carritoUsuario.splice(indice, 1)
-            localStorage.setItem('carrito', JSON.stringify(carritoUsuario))            
-            $(`#productoCarrito${indice}`).remove()
-
-            
-            if (carritoUsuario.length == 0) {
-                $('#divCarrito').append(`
-                    <p class="textGenParrafo"> No hay productos en el carrito.</p>
-                `
-                )
-            } else {
-                costo = costoFinal(carritoUsuario)
-                $('#divCarrito').append(`
-                    <p class="textGenParrafo"> ${detalleCostoFinal(costo)} </p>
-                `
-                )
-            }
+        // Click en el boton ELIMINAR - saco el producto del carrito
+        $(`#botonEliminar${productoEnCarrito[0].id}`).click(() => {
+            eliminarProducto(indice, carritoUsuario)
         })
 
+        // Click en el boton MAS - modifico la cantidad sumando
+        $(`#botonMas${productoEnCarrito[0].id}`).click(() => {
+            productoEnCarrito[1] += 1
+            localStorage.setItem('carrito', JSON.stringify(carritoUsuario))
+            valor = document.getElementById(`cantidad${productoEnCarrito[0].id}`)
+            valor.value = `${productoEnCarrito[1]}`
+            actualizoCostos(productoEnCarrito, carritoUsuario)
+        })
+
+        // Click en el boton MENOS - modifico la cantidad restando
+        $(`#botonMenos${productoEnCarrito[0].id}`).click(() => {
+            valor = document.getElementById(`cantidad${productoEnCarrito[0].id}`)
+            if (parseInt(valor.value) == 1) {
+                eliminarProducto(indice, carritoUsuario)
+            } else {
+                productoEnCarrito[1] -= 1
+                localStorage.setItem('carrito', JSON.stringify(carritoUsuario))
+                valor.value = `${productoEnCarrito[1]}`
+                actualizoCostos(productoEnCarrito, carritoUsuario)
+            }
+        })
     })
 
-
     $('#divCarrito').empty()
-    if (carritoUsuario.length == 0) {
-        $('#divCarrito').append(`
-            <p class="textGenParrafo"> No hay productos en el carrito.</p>
-        `
-        )
-    } else {
-        costo = costoFinal(carritoUsuario)
-        $('#divCarrito').append(`
-            <p class="textGenParrafo"> ${detalleCostoFinal(costo)} </p>
-        `
-        )
-    }
+    $('#divCarrito').append(detalleCarrito(carritoUsuario))
 
+    // Finalizo la compra
 
 })
+
+
+
+
+
 
 
 
---
+++ js/scriptProductos.js
@@ -0,0 +1,90 @@
+// Variables
+let remerasDisponibles = document.getElementById("remerasDisponibles")
+let sweatersDisponibles = document.getElementById("sweatersDisponibles")
+let camperasDisponibles = document.getElementById("camperasDisponibles")
+
+let enCarrito
+let posicionEnCarrito
+
+//  Funciones
+function cardProducto(producto) {
+    return (`
+    <div class="col-6 col-md-4 col-lg-3" id="producto${producto.id}">                    
+        <div class="centradoHoriz cardAlto">
+            <div class="cardEstilo" style="position: relative;">   
+                <div class="container mensaje" id="divAgregadoCorrecto${producto.id}" style="position: absolute;"></div>                         
+                <img src="${producto.imagen}" class="img-fluid bordeImagen" alt="${producto.nombre} ${producto.color}">
+                <div class="cardBody">
+                    <p class="card-text"> ${producto.nombre} ${producto.color}, ${producto.detalle} </p>
+                    <div class="centradoHoriz"> 
+                        <p class="card-text" style="margin-bottom : 0px"> $${producto.precio} </p>
+                        <button id="botonCarrito${producto.id}" class="textGenParrafo botonesCarrito"> <i class="bi bi-cart-fill"></i> </button>
+                    </div>                                 
+                </div>
+            </div> 
+        </div>                               
+    </div>
+    `)
+}
+
+function guardarCarrito(producto) {
+    $(`#divAgregadoCorrecto${producto.id}`).empty()
+    $(`#divAgregadoCorrecto${producto.id}`).append(`
+        <p> Se ha agregado correctamente 1 ${producto.nombre} al carrito. </p>
+        `
+    )
+    $(`#divAgregadoCorrecto${producto.id}`).slideDown(1000).delay(1000).fadeOut(1000)
+}
+
+function agregarEventListener(producto) {
+    enCarrito = false
+    document.getElementById(`botonCarrito${producto.id}`).addEventListener("click", () => {
+        $(`#divAgregadoCorrecto${producto.id}`).hide()
+
+        // miro si ya está en el carrito
+        carritoUsuario.forEach(productoEnCarrito => {
+            if (producto.id == productoEnCarrito[0].id) {
+                enCarrito = true
+                posicionEnCarrito = carritoUsuario.indexOf(productoEnCarrito)
+            }
+        })
+
+        // si no está en el carrito lo agrego
+        if (!enCarrito) {
+            carritoUsuario.push([producto, 1])
+            localStorage.setItem('carrito', JSON.stringify(carritoUsuario))
+            guardarCarrito(producto)
+
+        } else { // si está en el carrito, aviso que ya está y sumo
+            carritoUsuario[posicionEnCarrito][1] += 1
+            localStorage.setItem('carrito', JSON.stringify(carritoUsuario))
+            guardarCarrito(producto)
+        }
+    })
+}
+
+// agrego los productos a la página correspondiente
+fetch("./productos.json")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(producto => {
+            if (remerasDisponibles != null && producto.nombre == "Remera") {
+                remerasDisponibles.innerHTML += cardProducto(producto)
+            } else if (sweatersDisponibles != null && producto.nombre == "Sweater") {
+                sweatersDisponibles.innerHTML += cardProducto(producto)
+            } else if (camperasDisponibles != null && producto.nombre == "Campera") {
+                camperasDisponibles.innerHTML += cardProducto(producto)
+            }
+        })
+
+        data.forEach(producto => {
+            if (remerasDisponibles != null && producto.nombre == "Remera") {
+                agregarEventListener(producto)
+            } else if (sweatersDisponibles != null && producto.nombre == "Sweater") {
+                agregarEventListener(producto)
+            } else if (camperasDisponibles != null && producto.nombre == "Campera") {
+                agregarEventListener(producto)
+            }
+        })
+    });
+
